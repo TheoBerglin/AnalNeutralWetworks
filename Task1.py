82ff97042327d9cef18fe2d7e74fd26710908d19
Theo Berglin <theoberglin@outlook.com>
2017-09-11 16:23:05 +0200

    Created advanced algorithms for creating random patterns for evaluation of the sophisticated Hopfieldmodel

--- Task1.py
+++ Task1.py
@@ -1,5 +1,30 @@
+import random
 print("Adas äter otvättad röv till frukost")
 
 p = [x for x in range(0, 420, 20)]
 p[0] = 1
-print(p)
+nbr_of_bits = pow(10, 5)
+
+
+def get_pattern_value():
+    r = random.random()
+    if r < 0.5:
+        return -1
+    else:
+        return 1
+
+
+def get_pattern(p_size):
+    return [get_pattern_value() for i in range(0, p_size)]
+
+
+def create_random_patterns(nbr_of_patterns, p_size):
+    patterns = dict()
+    for i in range(0, nbr_of_patterns):
+        patterns[i] = get_pattern(p_size)
+    return patterns
+
+print(create_random_patterns(4, 10))
+
+
+
