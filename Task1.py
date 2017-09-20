85af4d4c1167bcde3d129bfeaf6d86fc2e2e7143
Theo Berglin <theoberglin@outlook.com>
2017-09-20 09:53:36 +0200

    Extraordinaire and advanced method for calculating the error function, opening results data and plotting the data. Code restructuring as well

--- Task1.py
+++ Task1.py
@@ -1,13 +1,16 @@
 import random
 from tqdm import tqdm
-from scipy.special import erf, erfc
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import csv
 print("Adas äter otvättad röv till frukost")
 
 p = [x for x in range(0, 420, 20)]
 # p = [x for x in range(20, 30, 1)]
 p[0] = 1
 N = 200
-nbr_of_bits = pow(10, 4)
+nbr_of_bits = pow(10, 5)
 
 
 def get_pattern_value():
@@ -83,12 +86,13 @@ def calculate_error_prob(nbr_of_patterns):
                 correct_sign_counter += N
             bit_counter += N
             if bit_counter >= nbr_of_bits:
+                print('Bit Counter: %d Correct sign counter: %d ' % (bit_counter, correct_sign_counter))
                 return 1 - correct_sign_counter / nbr_of_bits
     print('Bit Counter: %d Correct sign counter: %d '%( bit_counter, correct_sign_counter))
 
 
 
-def main():
+def generate_error_result():
     error_prob = []
     with open('results.txt', 'w') as file:
         for value in tqdm(p):
@@ -96,8 +100,38 @@ def main():
             error_prob.append(temp_error)
             file.write('%s\t%s\n' % (value, temp_error))
 
+def derived_error_function(N, P):
+    alpha = N/P
+    return 1/2*(1-math.erf((1+alpha)/math.sqrt(2*alpha)))
+def derived_error_function_alpha(alpha):
+    return 1/2*(1-math.erf((1+alpha)/math.sqrt(2*alpha)))
+
+def open_result_file():
+    x_values_generated = list()
+    y_values_generated = list()
+    with open('results.txt', 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        for row in spamreader:
+            x_values_generated.append(row[0])
+            y_values_generated.append(row[1])
+    return x_values_generated, y_values_generated
+
+def plot_main():
+    error_function_x = np.linspace(0.01, 2, 400)
+    error_function_y = [derived_error_function_alpha(alpha) for alpha in error_function_x]
+    error_function_y_max = max(error_function_y);
+    error_function_y = [y/error_function_y_max for y in error_function_y]
+    plt.plot(error_function_x, error_function_y)
+    x_values_generated, y_values_generated = open_result_file()
+    x_values_generated = [int(x)/N for x in x_values_generated]
+    plt.scatter(x_values_generated,y_values_generated)
+
+    plt.show()
 
 
 
 if __name__ == '__main__':
-    #main()
+
+    generate_error_result()
+    plot_main()
+
